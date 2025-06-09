eaf8627df09effb39fec6d58f603b477e1188c74
Yurii1960 <jura.ntu.most@gmail.com>
2025-06-09 19:43:20 +0300

    ДЗ 4-2 версія2

--- main42.py
+++ main42.py
@@ -1,9 +1,9 @@
 
 
 def get_cats_info(path):
-    file_name=path.name
+    
     try:
-        with open(file_name,'r',encoding='utf-8',) as fh:
+        with open(path.name,'r',encoding='utf-8',) as fh:
            
             cats_info=[]
             info_one_cat={}
@@ -16,6 +16,6 @@ def get_cats_info(path):
                 info_one_cat={'id':f'{id}','name':f'{name}','age':f'{age.strip()}'}
                 
                 cats_info.append(info_one_cat)
-            print(cats_info)
+        return cats_info
     except FileNotFoundError:
         print(f'Файл не знайдено')
